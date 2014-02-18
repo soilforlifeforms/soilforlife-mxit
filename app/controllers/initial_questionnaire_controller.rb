c26d565440123deb40894357fe7d6101422ced95
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2014-02-18 14:40:45 +0200

    add age and gender questions

--- app/controllers/initial_questionnaire_controller.rb
+++ app/controllers/initial_questionnaire_controller.rb
@@ -8,17 +8,40 @@ class InitialQuestionnaireController < ApplicationController
         mxit_form_session[:dummy] = 'TEST'
       end
 
-      step :trainer_name do
-        input :trainer_name, 'Trainer Name'
+      step :trainer do
+        input :trainer, 'Trainer'
       end
 
-      step :group_name do
-        input :group_name, 'Group Name'
+      step :group do
+        input :group, 'Group'
+      end
+
+      step :area do
+        input :area, 'Area'
+      end
+
+      step :age do
+        select :age, 'Choose a category that includes your age?', {
+          '< 18' => 'Under 18', 
+          '18-14' => '18-24',
+          '25-34' => '25-34',
+          '35-44' => '35-44',
+          '45-54' => '45-54',
+          '55-64' => '55-65',
+          '> 65' => '65 or Above',
+          'N/A' => 'Prefer Not to Answer'
+        }
+      end
+
+      step :gender do
+        select :gender, 'What is your gender?', {'male' => 'Male', 'female' => 'Female'}
       end
 
       step :done do 
-        @trainer_name = params[:trainer_name]
-        @group_name = params[:group_name]
+        @trainer = params[:trainer]
+        @group = params[:group]
+        @area = params[:area]
+        @gender = params[:gender]
         logger.info 'completed'
       end
       
