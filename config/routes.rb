afa4672da877f92ee84cc2c8428dc71c6c00e4e1
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2014-02-19 12:39:46 +0200

    add final questionaire

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,8 @@ Soilforlife::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   match '/initial(/:action)', :controller => :initial_questionnaire
+  match '/final(/:action)', :controller => :final_questionnaire
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140219091733) do
+ActiveRecord::Schema.define(:version => 20140219095302) do
+
+  create_table "final_questionnaires", :force => true do |t|
+    t.string   "overall_training_impression"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
+  end
 
   create_table "initial_questionnaires", :force => true do |t|
     t.string   "trainer"
