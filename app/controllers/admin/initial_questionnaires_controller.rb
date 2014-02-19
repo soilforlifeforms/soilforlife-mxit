4dec869d4222f35fe0071bafaa4b2b23b6efbe5d
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2014-02-19 13:20:10 +0200

    add bootstrap

--- app/controllers/admin/initial_questionnaires_controller.rb
+++ app/controllers/admin/initial_questionnaires_controller.rb
@@ -1,4 +1,7 @@
 class Admin::InitialQuestionnairesController < ApplicationController
+  layout 'admin'
+  before_filter :authenticate_user!
+
   def index
     @results = InitialQuestionnaire.all
   end
