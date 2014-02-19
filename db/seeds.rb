2d6bf390b44eab27b8fc41e838a552d8def80f9e
Jeanre Swanepoel <jeanre.swanepoel@ikhasi.co.za>
2014-02-19 12:45:20 +0200

    add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(
+  :email                 => "admin@test.com",
+  :password              => "12345678",
+  :password_confirmation => "12345678"
+)
+user.save!
